97931835158a486127bb3d2c88d822c98ebbd3eb
Wuningson <christdam55@gmail.com>
2019-11-01 16:24:25 +0100

    Added survived roure and modified app

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const sos = require('./routes/sos');
 const mia = require('./routes/mia');
+const survived = require('./routes/survived');
 
 app.use(express.urlencoded({extended: false}));
 
 app.use('/sos', sos);
 
 app.use('/mia', mia);
+
+app.use('/survived', survived);
 
 const port = 4444;
 
