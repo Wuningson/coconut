ce4c5ca6f1c2e41eef8f94404ec30206889b457b
Wuningson <christdam55@gmail.com>
2019-11-03 10:53:52 +0100

    Changes in app

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/mia', mia);
 
 app.use('/survived', survived);
 
-const port = 4444;
+const port = process.env.PORT || 4444;
 
 app.listen(port, ()=> console.log(`Server started at port ${port}`));
 
