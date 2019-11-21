a1ea4dbe924f0cd649686eceb267335e0e7fcd0b
Wuningson <christdam55@gmail.com>
2019-11-21 14:47:49 +0100

    Testing

--- api/routes/utils.js
+++ api/routes/utils.js
@@ -1,7 +1,30 @@
-const getCurrentDateTime = ()=> {
-  const currentdate = new Date();
-  const currentDateTime = currentdate.getDate() + "/" + (currentdate.getMonth()+1)  + "/" + currentdate.getFullYear() + " at " + currentdate.getHours() + ":"  + currentdate.getMinutes() + ":" + currentdate.getSeconds();
-  return currentDateTime;
+  const getCurrentDateTime = () => {
+  const currentDate = new Date()
+
+  let minutes = String(currentDate.getMinutes())
+  let seconds = String(currentDate.getSeconds())
+  let hours = currentDate.getHours() + 1
+
+  if (minutes.length < 2) minutes.padStart(2, '0')
+  if (seconds.length < 2) seconds.padStart(2, '0')
+
+  let meridian = hours < 12 ? 'A.M.' : 'P.M.'
+
+  const currentDateTime =
+    currentDate.getDate() +
+    '/' +
+    (currentDate.getMonth() + 1) +
+    '/' +
+    currentDate.getFullYear() +
+    ' at ' +
+    hours +
+    ':' +
+    minutes +
+    ':' +
+    seconds +
+    ` ${meridian}`
+  return currentDateTime
 }
 
 module.exports = getCurrentDateTime;
+
