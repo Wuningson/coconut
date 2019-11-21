cfa7e6f4a2d4b106c001592617c5145581cfc22c
Wuningson <christdam55@gmail.com>
2019-11-21 15:11:40 +0100

    Padded the time

--- api/routes/utils.js
+++ api/routes/utils.js
@@ -1,16 +1,16 @@
-  const getCurrentDateTime = () => {
+const getCurrentDateTime = () => {
   const currentDate = new Date()
 
   let minutes = String(currentDate.getMinutes())
   let seconds = String(currentDate.getSeconds())
   let hours = currentDate.getHours() + 1
 
-  if (minutes.length < 2) minutes.padStart(2, '0')
-  if (seconds.length < 2) seconds.padStart(2, '0')
+  if (minutes.length < 2) minutes = minutes.padStart(2, '0')
+  if (seconds.length < 2) seconds = seconds.padStart(2, '0')
 
   let meridian = hours < 12 ? 'A.M.' : 'P.M.'
 
-  if(hours>12) hours = hours%12;
+  if (hours > 12) hours = hours % 12
 
   const currentDateTime =
     currentDate.getDate() +
@@ -28,4 +28,6 @@
   return currentDateTime
 }
 
-module.exports = getCurrentDateTime;
+// module.exports = getCurrentDateTime;
+
+console.log(getCurrentDateTime())
